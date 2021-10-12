8f47246c2b63ecdac119b4d3ba156d379b4d90e7
sky-bai <569531262@qq.com>
2021-10-12 21:28:40 +0800

    管理员登陆

--- controller/admin_login.go
+++ controller/admin_login.go
@@ -2,6 +2,9 @@ package controller
 
 import (
 	"gateway/dto"
+	"github.com/e421083458/go_gateway/dao"
+	"github.com/e421083458/go_gateway/golang_common/lib"
+	"github.com/e421083458/go_gateway/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,12 +16,35 @@ func AdminLoginRegister(group *gin.RouterGroup) {
 	group.GET("/logout", adminLogin.AdminLoginOut)
 }
 
+// AdminLogin godoc
+// @Summary 管理员登陆
+// @Description 管理员登陆
+// @Tags 管理员接口
+// @ID /admin_login/login
+// @Accept  json
+// @Produce  json
+// @Param body body dto.AdminLoginInput true "body"
+// @Success 200 {object} middleware.Response{data=dto.AdminLoginOutput} "success"
+// @Router /admin_login/login [post]
 func (adminlogin *AdminLoginController) AdminLogin(c *gin.Context) {
 	params := &dto.AdminLoginInput{}
 	if err := params.BindValidParam(c); err != nil {
 		middleware.ResponseError(c, 2000, err)
 		return
 	}
+
+	//1. params.UserName 取得管理员信息 admininfo
+	//2. admininfo.salt + params.Password sha256 => saltPassword
+	//3. saltPassword==admininfo.password
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	admin := &dao.Admin{}
+
+	out := &dto.AdminLoginOutput{Token: admin.UserName}
+	middleware.ResponseSuccess(c, out)
 }
 
 func (adminlogin *AdminLoginController) AdminLoginOut(c *gin.Context) {
--- dto/admin_login.go
+++ dto/admin_login.go
@@ -1,6 +1,21 @@
 package dto
 
+import (
+	"github.com/e421083458/go_gateway/public"
+	"github.com/gin-gonic/gin"
+)
+
+// 管理员登陆请求结构体
 type AdminLoginInput struct {
 	UserName string `json:"username" form:"username" comment:"管理员用户名" example:"admin" validate:"required,valid_username"` //管理员用户名
 	Password string `json:"password" form:"password" comment:"密码" example:"123456" validate:"required"`                   //密码
+}
+
+func (param *AdminLoginInput) BindValidParam(c *gin.Context) error {
+	return public.DefaultGetValidParams(c, param)
+}
+
+// 管理员登陆回复结构体
+type AdminLoginOutput struct {
+	Token string `json:"token" form:"token" comment:"token" example:"token" validate:""` //token
 }
